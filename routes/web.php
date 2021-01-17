886fd5731ff78ae4bff080c59a6b03b34f5cb5ba
imahmedimtiaz <writetoahmedimtiaz@gmail.com>
2021-01-17 23:35:21 +0500

    changes in welcome page

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () { 
 Route::resource('quotes','QuotesController');
+});
