39efde6d322afe8caf44144906c45373210b7d89
Ahmed Imtiaz <ahmed.imtiaz@redsignal.biz>
2021-04-01 14:57:26 +0500

    changes in api functionality

--- app/Http/Controllers/Api/QuotesController.php
+++ app/Http/Controllers/Api/QuotesController.php
@@ -12,10 +12,20 @@ class QuotesController extends Controller
     public function all(Request $request)
     {
         $number = $request->number;
-         if($number != null && $number != ""){
-            $count =  Quote::count();
-            $take = $count - $number;
-            $quotes = Quote::with('Quotecategory','Quotesubcategory')->skip($number)->take($take)->get();
+        $count =  Quote::count();
+
+         if($number != null && $number != 0){
+           
+            if($count <= $number){
+                return response()->json([
+                    'status' => 442,
+                    'message' => 'Invalid number'
+                  ]);   
+            }else{
+                $take = $count - $number;
+                $quotes = Quote::with('Quotecategory','Quotesubcategory')->skip($number)->take($take)->get();
+            }
+         
          }else{
             $quotes =  Quote::with('Quotecategory','Quotesubcategory')->get();
          }
